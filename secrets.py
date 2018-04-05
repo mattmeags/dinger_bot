68a2b87cf0e366fe65c73cc82380bdb352ad2cf7
Matt Meagher <mattmeags8@Matts-MacBook-Pro.local>
2018-04-04 23:24:50 -0500

    Base functionality for bot, set up streaming, currently configured for my twitter will update

---
+++ secrets.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+consumer_key = '16zUE6DK1BFu3cvmbRT4MkBMb'
+consumer_secret = 'NRgoWm1s6NlnXjCuuWIdsAyuCIxW6ALTZv1vncwzrlDlV1OdGT'
+access_token = '153873253-QYO4rvZ4rUupU4XxzZvAWAQpYVsvjsVjb47Btslv'
+access_secret = '0uOgAZk5Jgsi0QCWMIYD8vTVwB9JDfeSv0UXl32WJ1MCl'
